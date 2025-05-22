ac1ac729f1f412160ff97d9c70f1693fede29c45
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-05-22 19:03:41 +0000

    added bill payment type

--- sh_payments/shreshta_payments/doctype/shpayment/shpayment.js
+++ sh_payments/shreshta_payments/doctype/shpayment/shpayment.js
@@ -12,3 +12,45 @@ frappe.ui.form.on("ShPayment", {
         }
     },
 });
+
+
+frappe.ui.form.on("ShBill", {
+    quantity: function (frm, cdt, cdn) {
+        updateRowTotal(cdt, cdn);
+        updateTotal(frm);
+    },
+    price: function (frm, cdt, cdn) {
+        updateRowTotal(cdt, cdn);
+        updateTotal(frm);
+    },
+    cgst: function (frm, cdt, cdn) {
+        updateRowTotal(cdt, cdn);
+        updateTotal(frm);
+    },
+    sgst: function (frm, cdt, cdn) {
+        updateRowTotal(cdt, cdn);
+        updateTotal(frm);
+    }
+});
+
+function updateRowTotal(cdt, cdn) {
+    const row = locals[cdt][cdn];
+    row.cgst = row.cgst || 0;
+    row.sgst = row.sgst || 0;
+    const grossAmount = row.price * row.quantity;
+    const gstRate = (row.cgst + row.sgst) / 100;
+    const netAmount = grossAmount + (grossAmount * gstRate);
+    frappe.model.set_value(cdt, cdn, 'amount', flt(netAmount));
+}
+
+function updateTotal(frm) {
+    let totalAmount = 0;
+
+    frm.doc.table_jmgk.forEach(row => {
+        const grossAmount = row.price * row.quantity;
+        const gstRate = (row.cgst + row.sgst) / 100;
+        const netAmount = grossAmount + (grossAmount * gstRate);
+        totalAmount += netAmount;
+    });
+    frm.set_value('total_amount', flt(totalAmount));
+}
