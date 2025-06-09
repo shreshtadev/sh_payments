efe5f711777c0d7f872a6fc4870d52908ad16b74
shreshtadev <chinmai.bharadwaj@ymail.com>
2025-06-09 10:55:08 +0000

    INIT Reports Repo

--- sh_payments/shreshta_payments/doctype/shcompany/shcompany.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, shreshtadev and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("ShCompany", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- sh_payments/shreshta_payments/report/daybookrep/daybookrep.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, shreshtadev and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["DayBookRep"] = {
-	"filters": [
-
-	]
-};
